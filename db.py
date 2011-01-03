2030a465673436efe4c328d126fc79ca932e33ac
gordonc <gchan@fastmail.fm>
2011-01-02 23:55:58 -0500

    Add modulename parameter to support any DB-API module and use 'sqlite3' modulename in stub testcase

--- db.py
+++ db.py
@@ -10,25 +10,23 @@ finally:
     if fp:
         fp.close()
 
-import pyodbc
 import threading
 import logging
 
 class DBPool():
-    def __init__(self,connectionstring,maxsize):
-        self.connectionstring = connectionstring
-        self.conns = [DBConnection_(socket_.socketpair(),connectionstring) for x in xrange(maxsize)]
-        self.threads = [threading.Thread(target=self.worker, args=(self.conns[x],)) for x in xrange(maxsize)]
-        self.queue = queue.Queue(maxsize)
-        for i in xrange(maxsize):
+    def __init__(self,connectionstring,poolsize,modulename='pyodbc'):
+        self.conns = [DBConnection_(socket_.socketpair()) for x in xrange(poolsize)]
+        self.threads = [threading.Thread(target=self.worker, args=(self.conns[x],)) for x in xrange(poolsize)]
+        self.queue = queue.Queue(poolsize)
+        for i in xrange(poolsize):
             self.threads[i].daemon = True
             self.threads[i].start()
+            self.conns[i].connect(connectionstring,modulename)
             self.queue.put(self.conns[i])
 
     def worker(self,conn):
         while True:
             conn.pipe[1].recv(1)
-
             try:
                 function = conn.state.function
                 args = conn.state.args
@@ -47,10 +45,12 @@ class DBConnection_():
     class State():
         pass
 
-    def __init__(self,pipe,connectionstring):
+    def __init__(self,pipe):
         self.pipe = pipe
-        self.conn = pyodbc.connect(connectionstring)
         self.state = self.State()
+
+    def connect(self,connectionstring,modulename):
+        self.conn = self.apply(__import__(modulename).connect,connectionstring)
 
     def __del__():
         self.conn.close()
@@ -80,7 +80,7 @@ class DBConnection():
         self.pool.queue.put(self.conn_)
 
     def cursor(self):
-        return DBCursor(self,self.conn_.conn.cursor())
+        return DBCursor(self,self.conn_.apply(self.conn_.conn.cursor))
 
 class DBCursor():
     def __init__(self,conn,cursor):
@@ -102,4 +102,14 @@ class DBCursor():
     @property
     def description(self):
         return self.cursor.description
+
+import unittest
+
+class TestDBPool(unittest.TestCase):
+    def test(self):
+        pool = DBPool(':memory:',4,'sqlite3')
+        conn = pool.get()
+
+if __name__ == '__main__':
+    unittest.main()
 
